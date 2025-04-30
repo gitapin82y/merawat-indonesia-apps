312b92c5a9ef5866195aed843518d10d8cd0bfe8
gitapin82y <gitapin82y@gmail.com>
2025-04-30 13:16:25 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,13 @@ use App\Http\Controllers\TripayPaymentMethodController;
 
 
 
+Route::get('/kampanye/{slug}/ref/{code}', [FundraisingController::class, 'showCampaignWithReferral'])->name('campaign.referral');
+
+
+Route::get('admin/kampanye/{slug}', [CampaignController::class, 'show'])->name('admin.campaign.detail');
+Route::get('kampanye/{slug}', [CampaignController::class, 'donaturKampanye'])->name('campaign.detail');
+
+
 Route::middleware(['checkRole:super_admin,yayasan'])->group(function () {
     Route::get('admin/edit-profile', function(){
         return view('admin.edit-profile');
@@ -39,12 +46,6 @@ Route::middleware(['checkRole:super_admin,yayasan'])->group(function () {
 
     Route::resource('kabar-pencairan', KabarPencairanController::class);
 });
-
-Route::get('/kampanye/{slug}/ref/{code}', [FundraisingController::class, 'showCampaignWithReferral'])->name('campaign.referral');
-
-
-Route::get('admin/kampanye/{slug}', [CampaignController::class, 'show'])->name('admin.campaign.detail');
-Route::get('kampanye/{slug}', [CampaignController::class, 'donaturKampanye'])->name('campaign.detail');
 
 
 
