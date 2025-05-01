cdeb49fdc0ae1aaf59c94c21b49677a3d80420a9
gitapin82y <gitapin82y@gmail.com>
2025-05-01 08:53:05 +0700

    sancutm

--- routes/api.php
+++ routes/api.php
@@ -4,18 +4,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DonationController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 
-// Tambahkan route callback Tripay di sini
 Route::post('/tripay/callback', [DonationController::class, 'callback'])->name('tripay.callback');
 
 Route::get('/api-test', function() {
