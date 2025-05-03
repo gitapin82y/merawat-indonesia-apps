08ff27f1c71c509a1adc6d16201df372df216899
gitapin82y <gitapin82y@gmail.com>
2025-05-03 15:42:05 +0700

    tes

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,10 @@ use App\Http\Controllers\DonationController;
 use App\Http\Middleware\TripayIpMiddleware;
 
 // Tripay callback route with IP middleware
-Route::post('/tripay/callback', [DonationController::class, 'callback']);
+Route::post('/tripay/callback', [DonationController::class, 'callback'])
+    ->name('tripay.callback')
+    ->middleware('tripay.ip');
+
 // Route for testing
 Route::get('/test-tripay', function () {
     return response()->json([
