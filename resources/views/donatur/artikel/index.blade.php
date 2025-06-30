90c06940ad6476ff66fd8e9916f1dfcecf8eb6b5
gitapin82y <gitapin82y@gmail.com>
2025-07-01 01:22:32 +0700

    perbaikan artikel

--- resources/views/donatur/artikel/index.blade.php
+++ resources/views/donatur/artikel/index.blade.php
@@ -8,7 +8,7 @@
 <div class="container p-3">
     <h5 class="mb-3">Artikel Terbaru</h5>
     @forelse($articles as $article)
-   <a href="{{ route('artikel.show', $article->slug) }}" class="col-12 row mt-2">
+   <a href="{{ url('artikel/'. $article->slug) }}" class="col-12 row mt-2">
     <div class="foto col-6 col-sm-6 col-md-4 align-items-center position-relative">
         <img src="{{ asset('storage/' . $article->image) }}" class="image-article-card w-100" alt="{{ $article->title }}">
     </div>
