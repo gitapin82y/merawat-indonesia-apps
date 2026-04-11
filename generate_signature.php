09382e6b6bea7dd6af2d5a981c00950942a8bd9f
gitapin82y <gitapin82y@gmail.com>
2026-04-11 21:29:02 +0700

    asd

--- generate_signature.php
+++ generate_signature.php
@@ -5,14 +5,35 @@ $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
 
 $timestamp = '2026-04-11T21:16:00+07:00';
 $method = 'POST';
-$endpoint = '/api/v1.0/qr/qr-mpm-generate';
+$endpoint = '/apimerchant/v1.0/debit/payment-host-to-host';
 $body = json_encode([
     'partnerReferenceNo' => 'QR-POS-006-' . time(),
     'merchantId'         => 'SGWYAYASANBINAMULIA',
+    'subMerchantId'      => '478e6640ee7aab15364bf42569559a35',
     'amount'             => ['value' => '25000.00', 'currency' => 'IDR'],
-    'validityPeriod'     => '2026-04-12T20:20:00+07:00',
-'pointOfInitiation'  => 'Website',
-    'additionalInfo'     => ['productCode' => 'SALDOMUQR','payType'     => 'REDIRECT',]
+    'urlParam'           => [
+        'url'        => 'https://merawatindonesia.com/donations/status',
+        'type'       => 'PAY_RETURN',
+        'isDeeplink' => 'N',
+    ],
+    'validUpTo'          => '2026-04-12T21:16:00+07:00',
+    'pointOfInitiation'  => 'Website',
+    'payOptionDetails'   => [
+        'payMethod'   => '008',
+        'payOption'   => 'SALDOMUQR',
+        'transAmount' => ['value' => '25000.00', 'currency' => 'IDR'],
+        'feeAmount'   => ['value' => '0.00', 'currency' => 'IDR'],
+    ],
+    'additionalInfo'     => [
+        'payType'    => 'REDIRECT',
+        'userName'   => 'Test Donatur',
+        'userEmail'  => 'test@merawatindonesia.com',
+        'userPhone'  => '081234567890',
+        'inquiryUrl' => 'https://merawatindonesia.com/api/v1.0/transfer-va/inquiry',
+        'paymentUrl' => 'https://merawatindonesia.com/api/v1.0/transfer-va/payment',
+        'callbackUrl'=> 'https://merawatindonesia.com/api/espay/callback',
+        'productCode'=> 'SALDOMUQR',
+    ],
 ], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
 
 $hashedBody = strtolower(hash('sha256', $body));
