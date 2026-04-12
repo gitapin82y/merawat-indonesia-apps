39645f2a142b78902402c1b62fb5b65304116de2
gitapin82y <gitapin82y@gmail.com>
2026-04-12 14:39:59 +0700

    asf

--- app/Http/Controllers/DonationController.php
+++ app/Http/Controllers/DonationController.php
@@ -634,11 +634,9 @@ protected function createTransaction($donation, $campaign)
             return ['success' => false, 'message' => 'Payment method not found'];
         }
 
-        if ($paymentMethod->category === 'qris') {
-    $result = $this->espayService->createQRMPM($donation, $campaign, $paymentMethod);
-} else {
-    $result = $this->espayService->createPaymentHostToHost($donation, $campaign, $paymentMethod);
-}
+     
+
+$result = $this->espayService->createPaymentHostToHost($donation, $campaign, $paymentMethod);
 
 if (isset($result['success']) && $result['success']) {
     $donation->snap_token = $result['data']['reference'];
