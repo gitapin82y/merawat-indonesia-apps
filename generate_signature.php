58750cc4b0584857484636afb5c3665bfc0751e8
gitapin82y <gitapin82y@gmail.com>
2026-04-12 15:02:46 +0700

    asd

--- generate_signature.php
+++ generate_signature.php
@@ -7,7 +7,7 @@ $timestamp = '2026-04-12T14:52:00+07:00';
 $method = 'POST';
 $endpoint = '/api/v1.0/qr/qr-mpm-generate';
 $body = json_encode([
-    'partnerReferenceNo' => 'DON-57626-1775979782',
+    'partnerReferenceNo' => 'DON-57628-1775980884',
     'merchantId'         => 'SGWYAYASANBINAMULIA',
     'amount'             => ['value' => '25000.00', 'currency' => 'IDR'],
     'additionalInfo'     => ['productCode' => 'QRIS'],
