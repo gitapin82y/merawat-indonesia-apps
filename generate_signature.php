1f94cf9beb30592621a789a75fa9411f9e7adecf
gitapin82y <gitapin82y@gmail.com>
2026-04-12 14:56:28 +0700

    asd

--- generate_signature.php
+++ generate_signature.php
@@ -3,7 +3,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 $app = require_once __DIR__ . '/bootstrap/app.php';
 $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
 
-$timestamp = '2026-04-12T14:32:00+07:00';
+$timestamp = '2026-04-12T14:52:00+07:00';
 $method = 'POST';
 $endpoint = '/api/v1.0/qr/qr-mpm-generate';
 $body = json_encode([
@@ -11,7 +11,7 @@ $body = json_encode([
     'merchantId'         => 'SGWYAYASANBINAMULIA',
     'amount'             => ['value' => '25000.00', 'currency' => 'IDR'],
     'additionalInfo'     => ['productCode' => 'QRIS'],
-    'validityPeriod'     => '2026-04-13T14:32:00+07:00',
+    'validityPeriod'     => '2026-04-13T14:52:00+07:00',
 ], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
 
 $hashedBody = strtolower(hash('sha256', $body));
