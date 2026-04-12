13628468d6754baf49fdacfdd26747cbfe056b55
gitapin82y <gitapin82y@gmail.com>
2026-04-12 15:05:40 +0700

    asd

--- generate_signature.php
+++ generate_signature.php
@@ -3,7 +3,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 $app = require_once __DIR__ . '/bootstrap/app.php';
 $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
 
-$timestamp = '2026-04-12T14:52:00+07:00';
+$timestamp = '2026-04-12T15:10:00+07:00';
 $method = 'POST';
 $endpoint = '/api/v1.0/qr/qr-mpm-generate';
 $body = json_encode([
