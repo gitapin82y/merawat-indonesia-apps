5c6a9461eb0b5211101780c8664d8038a3eb7adb
gitapin82y <gitapin82y@gmail.com>
2026-04-14 05:51:41 +0700

    signature

--- generate_signature.php
+++ generate_signature.php
@@ -3,37 +3,15 @@ require_once __DIR__ . '/vendor/autoload.php';
 $app = require_once __DIR__ . '/bootstrap/app.php';
 $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
 
-$timestamp = '2026-04-12T15:17:00+07:00';
+$timestamp = '2026-04-14T05:40:00+07:00';
 $method = 'POST';
-$endpoint = '/apimerchant/v1.0/debit/payment-host-to-host';
+$endpoint = '/api/v1.0/qr/qr-mpm-generate';
 $body = json_encode([
-    'partnerReferenceNo' => 'DON-57630-1775982745',
+    'partnerReferenceNo' => 'DON-57666-1776078007',
     'merchantId'         => 'SGWYAYASANBINAMULIA',
-    'subMerchantId'      => '478e6640ee7aab15364bf42569559a35',
     'amount'             => ['value' => '25000.00', 'currency' => 'IDR'],
-    'urlParam'           => [
-        'url'        => 'https://merawatindonesia.com/donations/57630/status',
-        'type'       => 'PAY_RETURN',
-        'isDeeplink' => 'N',
-    ],
-    'validUpTo'          => '2026-04-13T15:17:00+07:00',
-    'pointOfInitiation'  => 'Website',
-    'payOptionDetails'   => [
-        'payMethod'   => '014',
-        'payOption'   => 'BCAATM',
-        'transAmount' => ['value' => '25000.00', 'currency' => 'IDR'],
-        'feeAmount'   => ['value' => '0.00', 'currency' => 'IDR'],
-    ],
-    'additionalInfo'     => [
-        'payType'     => 'REDIRECT',
-        'userName'    => 'Test Donatur',
-        'userEmail'   => 'test@merawatindonesia.com',
-        'userPhone'   => '081234567890',
-        'inquiryUrl'  => 'https://merawatindonesia.com/api/v1.0/transfer-va/inquiry',
-        'paymentUrl'  => 'https://merawatindonesia.com/api/v1.0/transfer-va/payment',
-        'callbackUrl' => 'https://merawatindonesia.com/api/espay/callback',
-        'productCode' => 'BCAATM',
-    ],
+    'additionalInfo'     => ['productCode' => 'QRIS'],
+    'validityPeriod'     => '2026-04-15T05:40:00+07:00',
 ], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
 
 $hashedBody = strtolower(hash('sha256', $body));
