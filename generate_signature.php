2ce9b6c5f0768c7c3dd742c0bc43b5115f2fd400
gitapin82y <gitapin82y@gmail.com>
2026-04-16 18:57:16 +0700

    artisan

--- generate_signature.php
+++ generate_signature.php
@@ -7,15 +7,15 @@ $timestamp = '2026-04-15T18:21:00+07:00';
 $method = 'POST';
 $endpoint = '/api/v1.0/transfer-va/payment';
 $body = json_encode([
-    'partnerServiceId' => 'Espay',
-    'customerNo'       => 'SGWYAYASANBINAMULIA',
-    'virtualAccountNo' => 'DON-57632-1775985035',
+    'partnerServiceId'   => 'Espay',
+    'customerNo'         => 'SGWYAYASANBINAMULIA',
+    'virtualAccountNo'   => 'DON-57588-1775910522',
     'virtualAccountName' => 'apin',
-    'trxId'            => 'TRX-DOUBLE-001',
-    'paymentRequestId' => 'payreq-double-001',
-    'totalAmount'      => ['value' => '25000.00', 'currency' => 'IDR'],
-    'trxDateTime'      => '2026-04-15T18:21:00+07:00',
-    'additionalInfo'   => ['transactionStatus' => 'S'],
+    'trxId'              => 'TRX-DOUBLE-001',
+    'paymentRequestId'   => 'payreq-double-001',
+    'totalAmount'        => ['value' => '25000.00', 'currency' => 'IDR'],
+    'trxDateTime'        => '2026-04-15T18:21:00+07:00',
+    'additionalInfo'     => ['transactionStatus' => 'S'],
 ], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
 
 $hashedBody = strtolower(hash('sha256', $body));
